990803f76e888b240dd86cd94e947e15920ffe4e
Merve Babayiğit <ceren_626@hotmail.com>
2021-02-20 22:24:49 +0300

    MyDictionary

---
+++ Odev2/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Odev2
+{
+    class MyDictionary<MKey,MValue>
+    {
+        MKey[] keys;
+        MValue[] values;
+        public MyDictionary()
+        {
+            keys = new MKey[0];
+            values = new MValue[0];
+        }
+        public void Add(MKey key,MValue value)
+        {
+            MKey[] TempKeys = keys;
+            keys = new MKey[keys.Length+ 1];
+
+            MValue[] TempValues = values;
+            values = new MValue[values.Length + 1];
+            for (int i = 0; i < TempKeys.Length; i++)
+            {
+                keys[1] = TempKeys[i];
+
+                values[1] = TempValues[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+        public int Length
+        {
+            get { return keys.Length; }
+        }
+        public void List()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("Key : " + keys[i] + "values : " + values[i]);
+            }
+        }
+    }
+}
---
+++ Odev2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Odev2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,String> PcMarkalari = new MyDictionary<int,string>();
+            PcMarkalari.Add(1,"Casper");
+            PcMarkalari.Add(2,"Hp");
+            PcMarkalari.Add(3,"Dell");
+            PcMarkalari.Add(4,"Asus");
+            Console.WriteLine(PcMarkalari.Length);
+        }
+    }
+}
